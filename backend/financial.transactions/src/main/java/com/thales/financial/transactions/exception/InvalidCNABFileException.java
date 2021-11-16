664dbaebb13241d4b1f841543d6ccb84fbe22c50
Thales Melo da Silva <thalesmelodasilva@gmail.com>
2021-11-15 21:49:12 -0300

    alterando contrato do service, para considerar arquivo invalido

---
+++ backend/financial.transactions/src/main/java/com/thales/financial/transactions/exception/InvalidCNABFileException.java
@@ -0,0 +1,8 @@
+package com.thales.financial.transactions.exception;
+
+public class InvalidCNABFileException  extends RuntimeException {
+
+    public InvalidCNABFileException(String message) {
+        super(message);
+    }
+}
--- backend/financial.transactions/src/main/java/com/thales/financial/transactions/service/FinancialTransactionsService.java
+++ backend/financial.transactions/src/main/java/com/thales/financial/transactions/service/FinancialTransactionsService.java
@@ -1,12 +1,13 @@
 package com.thales.financial.transactions.service;
 
+import com.thales.financial.transactions.exception.InvalidCNABFileException;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface FinancialTransactionsService {
 
-    void batchInsertFromFile(MultipartFile file);
+    void batchInsertFromFile(MultipartFile file) throws InvalidCNABFileException;
 
     List getFinancialTransactions(int page, int size);
 
