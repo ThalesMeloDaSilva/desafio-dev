c46c3df43145e43f49c1b2e5defa3558c2d81034
Thales Melo da Silva <thalesmelodasilva@gmail.com>
2021-11-15 22:56:21 -0300

    adicionando beans

--- backend/financial.transactions/src/main/java/com/thales/financial/transactions/Application.java
+++ backend/financial.transactions/src/main/java/com/thales/financial/transactions/Application.java
@@ -1,13 +1,41 @@
 package com.thales.financial.transactions;
 
+import com.thales.financial.transactions.repository.FinancialTransactionsRepository;
+import com.thales.financial.transactions.repository.FinancialTransactionsRepositoryImpl;
+import com.thales.financial.transactions.service.FinancialTransactionsService;
+import com.thales.financial.transactions.service.FinancialTransactionsServiceImpl;
+import com.thales.financial.transactions.utils.FileParser;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@ComponentScan
+@EnableAutoConfiguration
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public FinancialTransactionsRepository financialTransactionsRepository(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+		return new FinancialTransactionsRepositoryImpl(namedParameterJdbcTemplate);
+	}
+
+	@Bean
+	public FileParser fileParser() {
+		return new FileParser();
+	}
+
+	@Bean
+	public FinancialTransactionsService financialTransactionsService(FinancialTransactionsRepository financialTransactionsRepository,
+																	 FileParser fileParser) {
+		return new FinancialTransactionsServiceImpl(financialTransactionsRepository, fileParser);
 	}
 
 }
